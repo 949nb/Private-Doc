cc38f5fd59af7687ef0d3e7bc4e7641d54421981
张含雨 <1661346771@qq.com>
2020-01-17 00:10:05 +0800

    组织页面层级

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,49 @@
 module.exports = {
   title: 'Hello VuePress',
-  description: 'Just playing around'
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [{
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'Guide',
+        link: '/guide/'
+      },
+      {
+        text: 'External',
+        link: 'https://google.com',
+        target: '_blank',
+        rel: ''
+      },
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [{
+            text: 'Chinese',
+            link: '/language/chinese/'
+          },
+          {
+            text: 'Japanese',
+            link: '/language/japanese/'
+          }
+        ]
+      }
+    ],
+    sidebar: [{
+        title: '介绍', // 必要的
+        path: '/zh/guide', // 可选的, 应该是一个绝对路径
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1, // 可选的, 默认值是 1
+        children: [
+          '/'
+        ]
+      },
+      {
+        title: 'Group 2',
+        children: [ /* ... */ ]
+      }
+    ],
+    smoothScroll: true
+  }
 }
