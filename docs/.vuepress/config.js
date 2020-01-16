f8c962b54828567763bde76141968b25b2815cd2
张含雨 <1661346771@qq.com>
2020-01-17 00:58:06 +0800

    页面基本搭建完毕

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
   themeConfig: {
+    sidebarDepth: 2,
     nav: [{
         text: 'Home',
         link: '/'
@@ -19,29 +20,49 @@ module.exports = {
       {
         text: 'Languages',
         ariaLabel: 'Language Menu',
-        items: [{
-            text: 'Chinese',
-            link: '/language/chinese/'
-          },
+        items: [
           {
-            text: 'Japanese',
-            link: '/language/japanese/'
+            text: 'Chinese',
+            link: '/'
           }
         ]
       }
     ],
-    sidebar: [{
+    lastUpdated: 'Last Updated',
+    sidebar: [
+      {
         title: '介绍', // 必要的
-        path: '/zh/guide', // 可选的, 应该是一个绝对路径
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1, // 可选的, 默认值是 1
+        children: ['/zh/guide']
+      },
+      {
+        title: '组件',
+        collapsable: false, // 可选的, 默认值是 true,
+        path: '/component/', // 可选的, 应该是一个绝对路径
+        collapsable: false,
         children: [
-          '/'
+          {
+            title: '类input组件', // 必要的
+            path: '/component/input/', // 可选的, 应该是一个绝对路径
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1, // 可选的, 默认值是 1
+            children: [
+              {
+                title: 'inputselect',
+                path: '/component/input/inputselect', // 可选的, 应该是一个绝对路径
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1, // 可选的, 默认值是 1
+              }
+            ]
+          },
+          // '/component/README'
         ]
       },
       {
-        title: 'Group 2',
-        children: [ /* ... */ ]
+        title: '工具函数',
+        collapsable: false, // 可选的, 默认值是 true,
+        children: ['/component/']
       }
     ],
     smoothScroll: true
